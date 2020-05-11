9e3420bd1eceede347782f4393bb7c40b79ed889
tailer954 <44743933+tailer954@users.noreply.github.com>
2020-05-11 12:10:58 +0800

    Subjects Print

---
+++ Small Project/01/01_subjects_print.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class subjectPrint 
+{
+	public static void main(String[] args) 
+	{
+		// Setup
+		Scanner keyboard = new Scanner(System.in);
+		int dataNum;
+		System.out.println("Enter Data Num :");
+		dataNum = keyboard.nextInt();
+		String subject[] = new String[dataNum];
+		int credit[] = new int[dataNum];
+		
+		// Input
+		System.out.println("Enter Subject Title(Note : Please change ' ' to '_')");
+		for (int i = 0; i < dataNum; i++)
+			subject[i] = keyboard.next();
+		System.out.println("Enter Subject Credit");
+		for (int i = 0; i < dataNum; i++)
+			credit[i] = keyboard.nextInt();
+
+		// Print
+		for (int i = 0; i < dataNum; i++)
+			System.out.println(subject[i] + "  " + credit[i]);
+	}
+}
